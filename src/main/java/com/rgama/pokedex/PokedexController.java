b0b6f59e5feb6a2bd725c89bc6f2e632845c184f
Rachel Gama <rachel.barato@hotmail.com>
2022-07-19 15:58:46 -0300

    refactor and add new methods to controller and service

--- src/main/java/com/rgama/pokedex/PokedexController.java
+++ src/main/java/com/rgama/pokedex/PokedexController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,12 +18,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PokedexController {
 	
 	@Autowired
-	private PokedexService<?> pokedexService;
+	private PokedexService pokedexService;
 	
-	PokedexController(PokedexService<?> pokedexService) {
+	PokedexController(PokedexService pokedexService) {
 		this.pokedexService = pokedexService;
 	}
 
+	
+	@PutMapping(path="/especies")
+	public @ResponseBody ResponseEntity<Especie> createNewEspecie(@RequestBody Especie newEspecie) {
+		return pokedexService.createNewEspecie(newEspecie);
+	}
 	
 	@GetMapping(path="/especies")
 	public @ResponseBody List<Especie> getAllEspecies() {
@@ -29,13 +36,18 @@ public class PokedexController {
 	}
 	
 	@GetMapping(path="/especies/{nome}")
-	public @ResponseBody ResponseEntity<Especie> getEspecieByName(@PathVariable String nome) {
+	public @ResponseBody Especie getEspecieByName(@PathVariable String nome) {
 		return pokedexService.getEspecieByName(nome);
 	}
 	
 	@DeleteMapping(path="/especies/{id}")
-	public @ResponseBody void deleteEspecie(Integer id) {
-		
+	public @ResponseBody ResponseEntity<String> deleteEspecie(Integer id) {
+		return pokedexService.deleteEspecie(id);
+	}
+	
+	@PutMapping(path="/pokemons")
+	public @ResponseBody ResponseEntity<Pokemon> createNewPokemon(@RequestBody Pokemon newPokemon) {
+		return pokedexService.createNewPokemon(newPokemon);
 	}
 	
 	@GetMapping(path="/pokemons")
@@ -43,18 +55,53 @@ public class PokedexController {
 		return pokedexService.getAllPokemons();
 	}
 	
-	@GetMapping(path="/pokemons/{id}")
-	public @ResponseBody Pokemon getPokemonById(@PathVariable Integer id) {
-		return pokedexService.getPokemonById(id);
-	}
-	
 	@DeleteMapping(path="/pokemons/{id}")
 	public void deletePokemon(@PathVariable Integer id) {
 		pokedexService.deletePokemon(id);
 	}
 	
+	@GetMapping(path="/pokemons/{id}")
+	public @ResponseBody Pokemon getPokemonById(@PathVariable Integer id) {
+		return pokedexService.getPokemonById(id);
+	}
+	
+	@PutMapping(path="/skills")
+	public @ResponseBody ResponseEntity<Skill> createNewSkill(@RequestBody Skill newSkill) {
+		return pokedexService.createNewSkill(newSkill);
+	}
+	
 	@GetMapping(path="/skills")
 	public @ResponseBody List<Skill> getAllSkills() {
 		return pokedexService.getAllSkills();
+	}
+	
+	@DeleteMapping(path="/skills/{id}")
+	public @ResponseBody ResponseEntity<String> deleteSkill(@PathVariable Integer id) {
+		return pokedexService.deleteSkill(id);
+	}
+	
+	@GetMapping(path="/skills/{id}")
+	public @ResponseBody Skill getSkillById(@PathVariable Integer id) {
+		return pokedexService.getSkillById(id);
+	}
+	
+	@PutMapping(path="/tipos")
+	public @ResponseBody ResponseEntity<TipoDePokemon> createNewTipoDePokemon(@RequestBody TipoDePokemon newTipo) {
+		return pokedexService.createNewTipo(newTipo);
+	}
+	
+	@GetMapping(path="/tipos")
+	public @ResponseBody List<TipoDePokemon> getAllTiposDePokemon() {
+		return pokedexService.getAllTiposDePokemon();
+	}
+	
+	@DeleteMapping(path="/tipos/{id}")
+	public @ResponseBody ResponseEntity<String> deleteTipoDePokemon(@PathVariable Integer id) {
+		return pokedexService.deleteTipoDePokemon(id);
+	}
+	
+	@GetMapping(path="/tipos/{id}")
+	public @ResponseBody TipoDePokemon getTipoDePokemonById(@PathVariable Integer id) {
+		return pokedexService.getTipoDePokemonById(id);
 	}
 }
--- src/main/java/com/rgama/pokedex/PokedexService.java
+++ src/main/java/com/rgama/pokedex/PokedexService.java
@@ -1,6 +1,7 @@
 package com.rgama.pokedex;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,33 +9,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PokedexService<T> {
-	
-	@Autowired
-	private PokemonRepository pokemonRepository;
+public class PokedexService {
 	
 	@Autowired
 	private EspecieRepository especieRepository;
 	
 	@Autowired
+	private PokemonRepository pokemonRepository;
+	
+	@Autowired
 	private SkillRepository skillRepository;
+	
+	@Autowired
+	private TipoDePokemonRepository tipoDePokemonRepository;
+
+	
+	public ResponseEntity<Especie> createNewEspecie(Especie newEspecie) {
+		return ResponseEntity.ok(especieRepository.save(newEspecie));
+	}
 	
 	public List<Especie> getAllEspecies() {
 		return especieRepository.findByOrderByEspecieAsc();
 	}
 	
-	public ResponseEntity<Especie> getEspecieByName(String nome) {
+	public Especie getEspecieByName(String nome) {
+		Optional<Especie> especieFound = Optional.of(especieRepository.findByEspecie(nome));
 		
-		if(!nome.isEmpty()) {
-			return ResponseEntity.ok(especieRepository.findByEspecie(nome));	
-		}
-		
-		throw new EspecieNotFoundException(nome);
+		return especieFound.orElseThrow(() -> new EntityNotFoundException("especie", nome));
 	}
 	
-	public ResponseEntity<T> deleteEspecie(Integer id) {
+	public ResponseEntity<String> deleteEspecie(Integer id) {
 		especieRepository.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Pokemon> createNewPokemon(Pokemon newPokemon) {
+		return  ResponseEntity.ok(pokemonRepository.save(newPokemon));
 	}
 	
 	public List<Pokemon> getAllPokemons() {
@@ -43,14 +53,47 @@ public class PokedexService<T> {
 	
 	public Pokemon getPokemonById(Integer id) {
 		return pokemonRepository.findById(id)
-				.orElseThrow(() -> new PokemonNotFoundException(id));
+				.orElseThrow(() -> new EntityNotFoundException("pokemon", id));
 	}
 	
-	public void deletePokemon(Integer id) {
+	public ResponseEntity<String> deletePokemon(Integer id) {
 		pokemonRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	public ResponseEntity<Skill> createNewSkill(Skill newSkill) {
+		return ResponseEntity.ok(skillRepository.save(newSkill));
+	}
+	
+	public ResponseEntity<String> deleteSkill(Integer id) {
+		skillRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 	public List<Skill> getAllSkills() {
 		return skillRepository.findAll();
+	}
+	
+	public Skill getSkillById(Integer id) {
+		return skillRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("skill", id));
+	}
+	
+	public ResponseEntity<TipoDePokemon> createNewTipo(TipoDePokemon newTipo) {
+		return ResponseEntity.ok(tipoDePokemonRepository.save(newTipo));
+	}
+	
+	public ResponseEntity<String> deleteTipoDePokemon(Integer id) {
+		tipoDePokemonRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	public List<TipoDePokemon> getAllTiposDePokemon() {
+		return tipoDePokemonRepository.findByOrderByTipoAsc();
+	}
+	
+	public TipoDePokemon getTipoDePokemonById(Integer id) {
+		return tipoDePokemonRepository.findById(id)
+				.orElseThrow(() -> new EntityNotFoundException("tipo de pokemon", id));
 	}
 }
